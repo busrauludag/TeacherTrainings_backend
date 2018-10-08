b9ad11721e8074cf5f79ba204d7ff0491716fe82
busrauludag <busrauludag13@gmail.com>
2018-10-08 22:31:13 +0300

    authorService created and express router added

--- main.js
+++ main.js
@@ -2,37 +2,20 @@ var express = require("express");
 var mongoose = require("mongoose");
 var bodyParser = require('body-parser');
 
-var Author = require('./models/author');
+var author = require('./services/authorService');
 
 var app = express();
 
 app.use(bodyParser.json());
 
 
-app.post('/author', (request,response) => {
-	var authorData = request.body;
-	var author = new Author(authorData);
-	author.save((error,result) => {
-		if(error){
-			console.log(error);
-			return response.status(500).send({message:error});
-		}
-
-		response.status(201);
-	});
-})
-
-
-app.get('/author', async(request,response) => {
-	var authors = await Author.find({}, '-__v');
-	response.send(authors);
-})
-
 mongoose.connect("mongodb://busra:b12345@ds245022.mlab.com:45022/teacherbase", (err) => {
 	if (!err) {
 		console.log("connected to db");
 	}
 })
+
+app.use('/author', author.router);
 
 app.listen(8080);
 
---
+++ services/authorService.js
@@ -0,0 +1,27 @@
+var express = require("express");
+var router = express.Router();
+
+var Author = require('../models/author');
+
+// /author/add ya da "" ile /author
+router.post('/add', (request,response) => {
+	var authorData = request.body;
+	var author = new Author(authorData);
+	author.save((error,result) => {
+		if(error){
+			console.log(error);
+			return response.status(500).send({message:error});
+		}
+
+		response.status(201);
+	});
+})
+
+// /author/list ya da "" ile /author
+router.get('/list', async(request,response) => {
+	var authors = await Author.find({}, '-__v');
+	response.send(authors);
+})
+
+var author = {router};
+module.exports = author;
