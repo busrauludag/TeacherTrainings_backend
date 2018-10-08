ad99fceb4021d5b1ea5d95f1b4955d9a25ad59f5
busrauludag <busrauludag13@gmail.com>
2018-10-08 21:57:57 +0300

    body-parser install and post data to db

--- main.js
+++ main.js
@@ -1,7 +1,25 @@
 var express = require("express");
 var mongoose = require("mongoose");
+var bodyParser = require('body-parser');
+
+var Author = require('./models/author');
 
 var app = express();
+
+app.use(bodyParser.json());
+
+app.post('/author', (request,response) => {
+	var authorData = request.body;
+	var author = new Author(authorData);
+	author.save((error,result) => {
+		if(error){
+			console.log(error);
+			return response.status(500).send({message:error});
+		}
+
+		response.status(201);
+	});
+})
 
 mongoose.connect("mongodb://busra:b12345@ds245022.mlab.com:45022/teacherbase", (err) => {
 	if (!err) {
@@ -10,3 +28,4 @@ mongoose.connect("mongodb://busra:b12345@ds245022.mlab.com:45022/teacherbase", (
 })
 
 app.listen(8080);
+
